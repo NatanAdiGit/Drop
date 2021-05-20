27846ecd6c604bb3921c61390dc4dc79be232a42
NatanAdi <natan.adi@mail.huji.ac.il>
2021-05-20 17:36:11 +0300

    nati - last one

--- app/src/main/java/huji/bez/drop/ScheduleActivity.kt
+++ app/src/main/java/huji/bez/drop/ScheduleActivity.kt
@@ -2,10 +2,41 @@ package huji.bez.drop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
+
+        val createSessionButton : FloatingActionButton = findViewById(R.id.addNewSessionButton)
+        val editSessionView : EditText = findViewById(R.id.newTask)
+        val startTimeButton : Button = findViewById(R.id.startTimeButton)
+        val endTimeButton : Button = findViewById(R.id.endTimeButton)
+        var startTime:String
+        var endTime :String
+
+        //todo : HH:MM
+
+        startTimeButton.setOnClickListener {
+
+        }
+
+        endTimeButton.setOnClickListener {
+
+        }
+
+
+
+
+
+
     }
+
+
+
+
+
 }
