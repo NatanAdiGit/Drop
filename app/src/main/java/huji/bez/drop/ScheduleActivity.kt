4b4657c54f22d9331593421dc84e46a4030c5e3e
INB <inbal.lavi1@mail.huji.ac.il>
2021-05-20 11:53:56 +0300

    added some activities

---
+++ app/src/main/java/huji/bez/drop/ScheduleActivity.kt
@@ -0,0 +1,11 @@
+package huji.bez.drop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
