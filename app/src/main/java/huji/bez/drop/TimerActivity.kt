c2e0c0d27c2b38452c064e131e9b5741eba56b18
INB <inbal.lavi1@mail.huji.ac.il>
2021-05-21 04:36:43 +0300

    added timer function - inbal

--- app/src/main/java/huji/bez/drop/TimerActivity.kt
+++ app/src/main/java/huji/bez/drop/TimerActivity.kt
@@ -1,11 +1,26 @@
 package huji.bez.drop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class TimerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
+
+        val timer: TextView = findViewById(R.id.timer)
+
+        object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timer.setText("seconds remaining: " + millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                timer.setText("done!")
+            }
+        }.start()
     }
 }
